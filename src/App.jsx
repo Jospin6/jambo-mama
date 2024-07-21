d0deeab6b1cd7f2d82f97ffb39a76b7a8ab623ca
jospin6 <jospinndagano1@gmail.com>
2024-07-21 12:47:33 +0200

    Install react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './containers/Home';
+
 function App() {
 
   return (
     <>
-      <div>Hello World</div>
+      <Routes>
+          <Route exact path="/" element={<Home/>} />
+      </Routes>
     </>
   )
 }
