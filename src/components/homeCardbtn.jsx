d3164f00c98ff98f93476f6305eebe2f74cd442a
jospin6 <jospinndagano1@gmail.com>
2024-07-22 15:30:19 +0200

    Add grids for home navigation

--- src/components/homeCardbtn.jsx
+++ src/components/homeCardbtn.jsx
@@ -1,6 +1,8 @@
-export const homeCardbtn = ({icon, text, onclick}) => {
-    return <div className="h-[300px] rounded-lg bg-blue-500 text-white shadow" onClick={onclick}>
-        <div className="flex justify-center"> {icon} </div>
-        <div className="flex justify-center"> {text} </div>
+export const HomeCardbtn = ({children, text, onclick}) => {
+    return <div className="h-[300px] flex justify-center items-center col-span-2 rounded-lg bg-blue-500 px-6 text-white shadow" onClick={onclick}>
+        <div>
+            <div className="flex justify-center text-[60px] pb-[20px]"> {children} </div>
+            <div className="flex justify-center text-center"> {text} </div>
+        </div>
     </div>
 }
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,5 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { HomeCardbtn } from '../components/homeCardbtn'
+
 export const Home = () => {
-    return <div>
-        Hello jn
+    return <div className="w-full h-[90vh] flex justify-center items-center">
+        <div className="grid w-full grid-cols-6 gap-4 h-[400px] px-[100px]">
+            <HomeCardbtn text={"Inscrivez-vous en tant que mère"}>
+                <FontAwesomeIcon icon={faUser}/>
+            </HomeCardbtn>
+            <HomeCardbtn text={"Inscrivez-vous en tant que fournisseur de soins de sante"}>
+                <FontAwesomeIcon icon={faUser}/>
+            </HomeCardbtn>
+            <HomeCardbtn text={"Inscrivez-vous en tant qu'agent de santé communauteure"}>
+                <FontAwesomeIcon icon={faUser}/>
+            </HomeCardbtn>
+        </div>
     </div>
 }
