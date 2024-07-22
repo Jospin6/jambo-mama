d780a32f6467cd5e40334b52d1165cbf2294fe14
jospin6 <jospinndagano1@gmail.com>
2024-07-22 09:57:09 +0200

    Create select field form

---
+++ src/components/SelectField.jsx
@@ -0,0 +1,14 @@
+export const SelectField = ({ labelText, fieldName, id, onchange, className, value, children }) => {
+    return <div className={`mt-2 ${className}`}>
+        <label htmlFor={id} className="block mb-[5px] bolder text-[16px]"> {labelText} </label>
+        <select
+            name={fieldName}
+            id={id}
+            className="w-full h-[35px] border-[1px] border-gray-400 
+                    rounded-lg pl-2 outline-none text-black"
+            onChange={onchange}
+            value={value}>
+                { children }
+        </select>
+    </div>
+}
