a3b80c8c87cd4a3826220a95edcfd28f103578e7
jospin6 <jospinndagano1@gmail.com>
2024-07-22 10:08:28 +0200

    Create sub banner component

---
+++ src/components/SubBanner.jsx
@@ -0,0 +1,6 @@
+export const SubBanner = ({children}) =>  {
+    return <div className="bg-[#EFE4B0] text-red-600 w-full h-[40px] flex 
+    justify-center items-center">
+        {children}
+    </div>
+}
