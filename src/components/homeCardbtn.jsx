74a72c14a27c4f29dcd3359005d2c5793e041052
jospin6 <jospinndagano1@gmail.com>
2024-07-22 09:19:22 +0200

    Add on click function to the home card btn to redirect user

--- src/components/homeCardbtn.jsx
+++ src/components/homeCardbtn.jsx
@@ -1,5 +1,5 @@
-export const homeCardbtn = ({icon, text}) => {
-    return <div className="h-[300px] rounded-lg bg-blue-500 text-white shadow">
+export const homeCardbtn = ({icon, text, onclick}) => {
+    return <div className="h-[300px] rounded-lg bg-blue-500 text-white shadow" onClick={onclick}>
         <div className="flex justify-center"> {icon} </div>
         <div className="flex justify-center"> {text} </div>
     </div>
