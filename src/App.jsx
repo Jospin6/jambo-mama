f61d4738a641676377478f761add4a89c58c07e5
jospin6 <jospinndagano1@gmail.com>
2024-07-22 09:11:16 +0200

    Create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './containers/Home';
+import { Navbar } from './components/Navbar';
 
 function App() {
 
   return (
     <>
+      <Navbar/>
       <Routes>
           <Route exact path="/" element={<Home/>} />
       </Routes>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+    return <div className="bg-[#EFE4B0] text-red-600 w-full h-[50px] flex 
+        justify-between items-center px-[100px]">
+        <div className="text-xl font-500">jamboMama</div>
+        <div></div>
+    </div>
+}
