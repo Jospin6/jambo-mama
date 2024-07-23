d2b85b5156012523ac8c9d29d216445bf23dd502
jospin6 <jospinndagano1@gmail.com>
2024-07-23 09:59:33 +0200

    Design mother home page

---
+++ src/containers/home_pages/MereHome.jsx
@@ -0,0 +1,38 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileEdit } from '@fortawesome/free-solid-svg-icons'
+import { HomeCardbtn } from '../components/HomeCardbtn'
+import { useDispatch } from 'react-redux'
+import { setNavbarTitle } from '../../features/navbar'
+import { useEffect } from 'react'
+import { SubBanner } from '../../components/SubBanner'
+import { PrincipalCardButton } from '../../components/PrincipalCardButton'
+
+export const Home = () => {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(setNavbarTitle("Inscrivez-vous en tant qu'agent"))
+    }, [])
+    return <div className="w-full h-[90vh] flex justify-center items-center">
+         <SubBanner>
+            PregnancyAge 36,6 BabyDueDate 12/08/2024
+        </SubBanner>
+        <div className="grid w-full grid-cols-4 gap-4 h-[400px] px-[100px]">
+          <PrincipalCardButton text="Follow your Pregnancy" className="col-span-2 bg-blue-500 mt-2">
+            <FontAwesomeIcon icon={faFileEdit}/>
+          </PrincipalCardButton>
+
+          <PrincipalCardButton text="Periodic Questionnaire" className="col-span-2 bg-purple-500 mt-2">
+            <FontAwesomeIcon icon={faFileEdit}/>
+          </PrincipalCardButton>
+
+          <PrincipalCardButton text="Vital update" className="col-span-2 bg-green-500 mt-2">
+            <FontAwesomeIcon icon={faFileEdit}/>
+          </PrincipalCardButton>
+
+          <PrincipalCardButton text="Something happened" className="col-span-2 bg-red-500 mt-2">
+            <FontAwesomeIcon icon={faFileEdit}/>
+          </PrincipalCardButton>
+        </div>
+    </div>
+}
