b5c86139900c8c76fd0dd5f7e7ef4c98b9db4260
jospin6 <jospinndagano1@gmail.com>
2024-08-01 17:51:39 +0200

    Change card icons

--- src/containers/home_pages/ProAccount.jsx
+++ src/containers/home_pages/ProAccount.jsx
@@ -1,11 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFileEdit } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faAddressBook, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 import { useDispatch } from 'react-redux'
 import { setNavbarTitle } from '../../features/navbar/navbarSlice'
 import { useEffect } from 'react'
 import { SubBanner } from '../../components/SubBanner'
 import { PrincipalCardButton } from '../../components/PrincipalCardButton'
 import { Link } from "react-router-dom"
+import JamboMamaNoCaptionWhite from '../../assets/JamboMamaNoCaptionWhite.png'
 
 export const ProAccount = () => {
     const dispatch = useDispatch()
@@ -16,26 +17,26 @@ export const ProAccount = () => {
     return <div className="w-full h-[90vh] flex justify-center items-center">
         <div className="grid w-[80%] grid-cols-4 gap-4">
             <Link to={"/fournisseur-patients"} className='col-span-2 mt-2'>
-                <PrincipalCardButton text="Patients" className="bg-blue-500">
-                    <FontAwesomeIcon icon={faFileEdit} />
+                <PrincipalCardButton text="Patients" className="bg-green-500">
+                    <img src={JamboMamaNoCaptionWhite} alt="mama" className='w-[60px] h-[60px]' />
                 </PrincipalCardButton>
             </Link>
 
             <Link to={"/fournisseur-patients"} className='col-span-2 mt-2'>
-                <PrincipalCardButton text="Directory" className="bg-purple-500">
-                    <FontAwesomeIcon icon={faFileEdit} />
+                <PrincipalCardButton text="Directory" className="bg-red-500">
+                    <FontAwesomeIcon icon={faAddressBook} />
                 </PrincipalCardButton>
             </Link>
 
             <Link to={"/fournisseur-instructions"} className='col-span-2 mt-2'>
-                <PrincipalCardButton text="Instructions" className="bg-green-500">
-                    <FontAwesomeIcon icon={faFileEdit} />
+                <PrincipalCardButton text="Instructions" className="bg-blue-500">
+                    <FontAwesomeIcon icon={faInfoCircle} />
                 </PrincipalCardButton>
             </Link>
 
             <Link to={"/mon-compte"} className='col-span-2 mt-2'>
-                <PrincipalCardButton text="My Account" className="bg-red-500">
-                    <FontAwesomeIcon icon={faFileEdit} />
+                <PrincipalCardButton text="My Account" className="bg-purple-500">
+                    <FontAwesomeIcon icon={faUser} />
                 </PrincipalCardButton>
             </Link>
         </div>
