53d36b3f9f5a16bd4d2143c400daefd1aa2f985b
jospin6 <jospinndagano1@gmail.com>
2024-08-01 17:03:48 +0200

    Redesign the questionItem component

--- src/components/QuestionItem.jsx
+++ src/components/QuestionItem.jsx
@@ -1,16 +1,16 @@
 export const QuestionItem = ({question, isHidden, icon, otherText}) => {
     return <div className="border-[1px] border-gray-400 rounded-lg p-2 mb-2">
-        <div className="flex justify-between">
+        <div className="flex justify-between text-[14px]">
             <div> <span> {icon} </span> <span>{question}</span> </div>
             <span>*</span>
         </div>
 
-        <div>
-            <button className="px-[10px] py-[2px] rounded-lg bg-green-500 mr-2 text-white text-[14px]">Fine</button>
-            <button className="px-[10px] py-[2px] rounded-lg bg-red-500 mr-2 text-white text-[14px]">So-so</button>
-            <button className={`px-[10px] py-[2px] rounded-lg bg-blue-500 mr-2 text-white text-[14px] ${isHidden}`}>{otherText}</button>
+        <div className="pt-2">
+            <button className="px-[10px] py-[2px] border-[1px] border-green-500 rounded-lg mr-2 text-green-500 text-[14px]">Fine</button>
+            <button className="px-[10px] py-[2px] border-[1px] border-red-500 rounded-lg mr-2 text-red-500 text-[14px]">So-so</button>
+            <button className={`px-[10px] py-[2px] border-[1px] border-blue-500 rounded-lg mr-2 text-blue-500 text-[14px] ${isHidden}`}>{otherText}</button>
         </div>
 
-        <div className="text-[14px] text-blue-500">Histore</div>
+        <div className="text-[12px] text-blue-500">Histore</div>
     </div>
 }
